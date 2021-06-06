5c2896bf639e02a5edcca9d8c6f5ccb00fe4395e
TMFV <denys.tymofieiev@gmail.com>
2021-06-07 00:04:47 +0300

    fix table

--- contacts.js
+++ contacts.js
@@ -4,16 +4,14 @@ const shortid = require("shortid");
 
 const contactsPath = path.relative("./", "./db/contacts.json");
 
-// TODO: задокументировать каждую функцию
 async function listContacts() {
   let list;
   await fs.readFile(contactsPath, "utf8").then((data) => {
     list = JSON.parse(data);
   });
-  console.log(list);
+  console.table(list);
   return list;
 }
-//listContacts();
 
 async function getContactById(contactId) {
   let findContact;
@@ -24,20 +22,16 @@ async function getContactById(contactId) {
   console.log(findContact);
   return findContact;
 }
-//getContactById(7);
 
 async function removeContact(contactId) {
-  // ...твой код
   let newListAfterRemove;
   const allContacts = await listContacts();
   newListAfterRemove = allContacts.filter(
     (currentContact) => currentContact.id !== contactId
   );
   await fs.writeFile(contactsPath, JSON.stringify(newListAfterRemove));
-  console.log(newListAfterRemove);
+  console.table(newListAfterRemove);
 }
-
-//removeContact(3);
 
 async function addContact(name, email, phone) {
   const allContacts = await listContacts();
@@ -49,10 +43,8 @@ async function addContact(name, email, phone) {
   };
   allContacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(allContacts));
-  console.log(allContacts);
-  // ...твой код
+  console.table(allContacts);
 }
-//addContact("John", "john@email", "777-77-77");
 
 module.exports = {
   listContacts,
--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const contacts = require("./contacts");
-const yargs = require("yargs");
-
-// index.js
 const argv = require("yargs").argv;
 
-// TODO: рефакторить
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
