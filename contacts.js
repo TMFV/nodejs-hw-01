936a5860e6b3a0d5c25b834f53e382e7e934d7d0
TMFV <denys.tymofieiev@gmail.com>
2021-06-07 11:38:18 +0300

    added catching errors

--- contacts.js
+++ contacts.js
@@ -6,44 +6,59 @@ const contactsPath = path.relative("./", "./db/contacts.json");
 
 async function listContacts() {
   let list;
-  await fs.readFile(contactsPath, "utf8").then((data) => {
-    list = JSON.parse(data);
-  });
+  await fs
+    .readFile(contactsPath, "utf8")
+    .then((data) => {
+      list = JSON.parse(data);
+    })
+    .catch((error) => console.error(error));
   console.table(list);
   return list;
 }
 
 async function getContactById(contactId) {
-  let findContact;
-  const allContacts = await listContacts();
-  findContact = await allContacts.filter(
-    (currentContact) => currentContact.id === contactId
-  );
-  console.log(findContact);
-  return findContact;
+  try {
+    let findContact;
+    const allContacts = await listContacts();
+    findContact = await allContacts.filter(
+      (currentContact) => currentContact.id === contactId
+    );
+    console.log(findContact);
+    return findContact;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function removeContact(contactId) {
-  let newListAfterRemove;
-  const allContacts = await listContacts();
-  newListAfterRemove = allContacts.filter(
-    (currentContact) => currentContact.id !== contactId
-  );
-  await fs.writeFile(contactsPath, JSON.stringify(newListAfterRemove));
-  console.table(newListAfterRemove);
+  try {
+    let newListAfterRemove;
+    const allContacts = await listContacts();
+    newListAfterRemove = allContacts.filter(
+      (currentContact) => currentContact.id !== contactId
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(newListAfterRemove));
+    console.table(newListAfterRemove);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function addContact(name, email, phone) {
-  const allContacts = await listContacts();
-  let newContact = {
-    id: shortid.generate(),
-    name: name,
-    email: email,
-    phone: phone,
-  };
-  allContacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(allContacts));
-  console.table(allContacts);
+  try {
+    const allContacts = await listContacts();
+    let newContact = {
+      id: shortid.generate(),
+      name: name,
+      email: email,
+      phone: phone,
+    };
+    allContacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+    console.table(allContacts);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
